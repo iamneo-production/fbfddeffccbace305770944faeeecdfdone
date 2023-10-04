1d26f955d0ed7ee5b555f8eb1df82d81d0348b67
Iamneo <techadmin@iamneo.ai>
2023-10-04 18:28:03 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/examly/springapp/controller/LaptopController.java
@@ -0,0 +1,11 @@
+package com.examly.springapp.controller;
+
+import com.examly.springapp.model.Laptop;
+import com.examly.springapp.service.ApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@RestController
+public class LaptopController {
+    
+}
